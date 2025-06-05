70dc5681867500701abdff837e033d86634edb6c
Mahdi Albori <mahdialbori@gmail.com>
2025-06-05 10:55:34 -0400

    simplified soltion using the strings.Fields() method

--- repl.go
+++ repl.go
@@ -6,15 +6,5 @@ import (
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(strings.TrimSpace(text))
-	words := strings.Split(text, " ")
-
-	//Remove empty strings resulting from multiple spaces
-	res := []string{}
-
-	for _, word := range words {
-		if word != "" {
-			res = append(res, word)
-		}
-	}
-	return res
+	return strings.Fields(text)
 }
