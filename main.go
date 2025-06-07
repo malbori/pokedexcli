84b95325ba6e32c37bfb4d496c28b3e77076371b
Mahdi Albori <mahdialbori@gmail.com>
2025-06-06 22:00:44 -0400

    refactored main and flow of repl

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("Pokedex > ")
-
-	for scanner.Scan() {
-
-		userInput := scanner.Text()
-		cleaned := cleanInput(userInput)
-
-		fmt.Printf("Your command was: %s\n", cleaned[0])
-		fmt.Print("Pokedex > ")
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		userInput := scanner.Text()
+		cleaned := cleanInput(userInput)
+
+		if len(cleaned) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", cleaned[0])
+
+	}
+}
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(strings.TrimSpace(text))
