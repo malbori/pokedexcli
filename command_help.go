c3d9aa9725a5f9d7a3f8db04303be9e4bf4b9a16
Mahdi Albori <mahdialbori@gmail.com>
2025-06-11 22:44:08 -0400

    dynamically generating help message now so its always up to date whenever new commands are registered

--- command_help.go
+++ command_help.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func commandHelp() error {
-	helpMessage := "Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex\n"
+	helpMessage := "Welcome to the Pokedex!\nUsage:\n\n"
 	fmt.Print(helpMessage)
+
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
 	return nil
 }
